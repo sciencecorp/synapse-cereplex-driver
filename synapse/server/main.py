62da1084ffdbdad7222e37fbb919f8479aa335b2
Max Hodak <maxhodak@gmail.com>
2024-06-25 23:09:35 -0700

    use SO_REUSEPORT properly to allow multiple servers to run

--- synapse/server/main.py
+++ synapse/server/main.py
@@ -66,12 +66,12 @@ if __name__ == "__main__":
     loop.set_debug(True)
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     sock.bind(("", args.discovery_port))
     group = socket.inet_aton(args.discovery_addr)
     mreq = struct.pack("4sL", group, socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
     listen = loop.create_datagram_endpoint(
         lambda: MulticastDiscoveryProtocol(
