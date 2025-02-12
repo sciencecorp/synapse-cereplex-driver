89f01a895541da1061fddeb4b59908f9649fe8d2
Antonia Elsen <antoniaelsen@gmail.com>
2025-02-11 17:26:44 -0800

    chore: remove Fakebrain (not relevant to this repo)

--- synapse_cereplex/__main__.py
+++ synapse_cereplex/__main__.py
@@ -5,10 +5,6 @@ from synapse_cereplex.electrical_broadband import (
     ElectricalBroadband,
     PERIPHERALS as ElectricalBroadbandPeripherals,
 )
-from synapse_cereplex.fakebrain_broadband import (
-    FakeBrainBroadband,
-    PERIPHERALS as FakeBrainBroadbandPeripherals,
-)
 from synapse.api.node_pb2 import NodeType
 
 defaults = ENTRY_DEFAULTS.copy()
